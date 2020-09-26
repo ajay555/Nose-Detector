fd738ebfcb0847b1137a1bbf8545e28e2596bc9a
Ajay Singh <ajay555@gmail.com>
2020-09-26 15:47:08 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let eyelX = 0;
 let eyelY = 0;
  
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(800, 800);
   video = createCapture(VIDEO);
   video.hide();
   poseNet = ml5.poseNet(video, modelReady);
